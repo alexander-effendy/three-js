987092ca855e984cb177c2f1f815e4315a751dad
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-04-25 00:32:30 +1000

    setup: npm i three, fibre, drei

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="grid place-items-center h-screen w-screen bg-gray-700 text-2xl text-white">Hello World</div>
+      <div className="grid place-items-center h-screen w-screen bg-gray-700 text-2xl text-white">Ready to learn three.js</div>
     </>
   )
 }
